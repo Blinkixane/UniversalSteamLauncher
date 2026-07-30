888b3c059eae3a708c9eac567bbcbef730d1c082
Blinkixane <143320827+Blinkixane@users.noreply.github.com>
2026-07-30 18:12:54 +0200

    Clean up useless comments in Get-Aumid.ps1

--- tools/Get-Aumid.ps1
+++ tools/Get-Aumid.ps1
@@ -1,18 +1,3 @@
-# Get-Aumid.ps1
-#
-# Aide a retrouver l'AUMID (AppUserModelID) d'un jeu deja lance au moins une
-# fois, a coller dans LauncherGenerator si la detection automatique echoue.
-# Get-Aumid.ps1
-#
-# Helps find the AUMID (AppUserModelID) of a game that has been launched at
-# least once, to paste into LauncherGenerator if automatic detection fails.
-#
-# Usage :
-#   1. Lance le jeu une fois / Launch the game once.
-#   2. powershell -ExecutionPolicy Bypass -File Get-Aumid.ps1
-#   3. Tape (une partie de) son nom / Type (part of) its name.
-#   4. Copie la valeur AppID affichee / Copy the displayed AppID value.
-
 $name = Read-Host "Nom du jeu / Game name"
 
 $matches = Get-StartApps | Where-Object { $_.Name -like "*$name*" }
